0579868af3aa70ab4066f716ec8c5f141ff61ca3
Ryszard1989 <rys.szulc@gmail.com>
2021-05-17 17:29:11 +0100

    included anagram checker and generated words

--- gameManager.py
+++ gameManager.py
@@ -1,4 +1,13 @@
 from wordChecker import *
+import string
+import random
+
+def generateRandomWord(wordLength):
+    lowerLetters = string.ascii_lowercase
+    generatedWord = ""
+    for x in range(0, wordLength):
+        generatedWord += random.choice(lowerLetters)
+    return generatedWord
 
 class GameManager:
     def __init__(self):
@@ -10,9 +19,14 @@ class GameManager:
         print("new game started")
         userEnteredWord = ""
         while userEnteredWord != "Quit":
+            generatedWord = generateRandomWord(10)
+            print("")
+            print("Make a word from: " + generatedWord)
             userEnteredWord = raw_input("Enter word: ")
-            wordValue = self.wordChecker.checkWord(userEnteredWord)
-            print (wordValue)
+            wordValue = self.wordChecker.checkWord(userEnteredWord, generatedWord)
+            print()
+            print ("Word score: " + str(wordValue))
+            self.currentScore += wordValue
+            print ("Current Score: " + str(self.currentScore))
+        print ("Final Score: " + str(self.currentScore))
 
-    #def generateRandomWord(self, wordLength):
-    #    for 1:wordLength:
--- wordChecker.py
+++ wordChecker.py
@@ -1,13 +1,31 @@
 import json
 
+def isValidAnagram(proposedWord, generatedWord):
+    sortedProposedWord = sorted(proposedWord)
+    sortedGeneratedWord = sorted(generatedWord)
+    # using issubset() to
+    # check subset of list
+    flag = 0
+    if set(sortedProposedWord).issubset(set(sortedGeneratedWord)):
+        flag = 1
+    # printing result
+    if (flag):
+        print (proposedWord + " is valid anagram")
+        return True
+    else:
+        print (proposedWord + " is not a valid anagram")
+        return False
+
 class WordChecker:
     def __init__(self):
         with open('words_dictionary.json') as json_file:
             self.possibleWords = json.load(json_file)
 
-    def checkWord(self, proposedWord):
-        if self.isValidWord(proposedWord):
-            return(len(proposedWord))
+    def checkWord(self, proposedWord, generatedWord):
+        if isValidAnagram(proposedWord, generatedWord):
+            if self.isValidWord(proposedWord):
+                return len(proposedWord)
+            print(proposedWord + " is not a valid english word!")
         return 0
 
     def isValidWord(self,proposedWord):
